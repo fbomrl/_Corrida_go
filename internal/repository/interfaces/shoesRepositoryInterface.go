fbd1a2bbfda04dd855183b02cac4f4a15b65e02b
Fabio Meireles <fbomrl14@gmail.com>
2025-09-08 20:14:43 -0300

    Inclusão de func na interface

--- internal/repository/interfaces/shoesRepositoryInterface.go
+++ internal/repository/interfaces/shoesRepositoryInterface.go
@@ -4,4 +4,6 @@ import "github.com/fbomrl/_Corrida_go/internal/model"
 
 type ShoesRepositoryInterface interface {
 	CreateShoes(shoes model.Shoes) error
+	FindShoesById(id int) (*model.Shoes, error)
+	FindAllShoes() ([]*model.Shoes, error)
 }
